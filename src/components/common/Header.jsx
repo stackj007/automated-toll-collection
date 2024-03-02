7b0f991bdcc82fc9241a2fb51c49951d2622dae9
Ahmed Gamal <agamal0077@gmail.com>
2024-03-02 17:34:01 +0200

    - Styling Hero section - Extracted header code into a separate Header component

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,44 @@
+import styles from './Header.module.css'
+import Logo from '../../assets/Logo/Toll Express 1.png'
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt="Your company logo" />
+      <nav>
+        <ul>
+          <li>
+            <a href="#" className={styles.navLink}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.navLink}>
+              Account
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.navLink}>
+              Tolls
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.navLink}>
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.authButtons}>
+        <button className={styles.loginButton}>
+          Login
+        </button>
+        <button className={styles.signupButton}>
+          Sign Up
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/components/pages/LandingPage/LandingPage.jsx
+++ src/components/pages/LandingPage/LandingPage.jsx
@@ -1,56 +1,65 @@
+import Header from '../../../components/common/Header'
 import styles from './LandingPage.module.css'
-import Logo from '../../../assets/Logo/Toll Express 1.png'
+import accountIcon from '../../../assets/icons/account 1.png'
+import calculator from '../../../assets/icons/calculator 1.png'
+import payment from '../../../assets/icons/payment 1.png'
+import toll from '../../../assets/icons/toll (1) 1.png'
+
 function LandingPage() {
   return (
     <div className={styles.landingPage}>
-      <header className={styles.header}>
-        <img src={Logo} alt="Your company logo" />
+      <Header />
 
-        <nav>
-          <ul>
-            <li>
-              <a href="#" className={styles.navLink}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.navLink}>
-                Account
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.navLink}>
-                Tolls
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.navLink}>
-                Contact Us
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.authButtons}>
-          <button className={styles.loginButton}>
-            Login
-          </button>
-          <button className={styles.signupButton}>
-            Sign Up
-          </button>
-        </div>
-      </header>
       <main className={styles.main}>
         <section className={styles.hero}>
-          <h2>Enjoy the ride.</h2>
-          <p>
-            Ready to handle toll payments hassle-free? Join
-            now and access all the resources needed to
-            streamline your toll payment experience while
-            cruising our roads.
-          </p>
-          <button className={styles.button}>Sign Up</button>
+          <div className={styles.overlay}>
+            <div className={styles.overlayContent}>
+              {' '}
+              <h1>Enjoy the ride.</h1>
+              <p>
+                Ready to handle toll payments hassle-free?
+                Join now and access all the resources needed
+                to streamline your toll payment experience
+                while cruising our roads.
+              </p>
+              <div className={styles.benefits}>
+                <div className={styles.subbenefits}>
+                  <img
+                    src={accountIcon}
+                    alt="Account Icon"
+                  />
+                  <p>Save time and money</p>
+                </div>
+
+                <div className={styles.subbenefits}>
+                  <img
+                    src={calculator}
+                    alt="Account Icon"
+                  />
+                  <p>
+                    calculate the toll for any trip on the
+                    toll roads
+                  </p>
+                </div>
+
+                <div className={styles.subbenefits}>
+                  <img src={toll} alt="Account Icon" />
+                  <p>
+                    Pay or learn more about a Notice of toll
+                    violation
+                  </p>
+                </div>
+
+                <div className={styles.subbenefits}>
+                  <img src={payment} alt="Account Icon" />
+                  <p>Numerous payment methods</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
-        <section className={styles.benefits}>
+
+        {/* <section className={styles.benefits}>
           <h3>Save time and money</h3>
           <p>
             Our automated toll collection system eliminates
@@ -62,8 +71,9 @@ function LandingPage() {
             Choose from a variety of convenient payment
             methods to keep your account current.
           </p>
-        </section>
-        <section className={styles.actions}>
+        </section> */}
+
+        {/* <section className={styles.actions}>
           <a href="#" className={styles.actionLink}>
             Pay or learn more about a Notice of Toll
             Violation
@@ -72,7 +82,7 @@ function LandingPage() {
             Calculate the toll for any trip on our toll
             roads
           </a>
-        </section>
+        </section> */}
       </main>
       <footer className={styles.footer}>
         <p>
