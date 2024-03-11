ab51dd735858618f5231b3132e3581a87bd369cf
Ahmed Gamal <agamal0077@gmail.com>
2024-03-11 21:41:19 +0200

    configuring project to handle client-side routing properly

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,18 +1,18 @@
-import "reflect-metadata"
-import {DataSource} from "typeorm"
-import {User} from "./entity/User"
-import {Migration1709658629562} from "./migration/1709658629562-migration";
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { User } from './entity/User'
+import { Migration1709658629562 } from './migration/1709658629562-migration'
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "root",
-    database: "TollCollection",
-    synchronize: true,
-    logging: true,
-    entities: [User],
-    migrations: [Migration1709658629562],
-    subscribers: [],
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'TollCollection',
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  migrations: [Migration1709658629562],
+  subscribers: [],
 })
