3bb712a79000bcfff82cbbc0ab15cd11cba49946
blank <blankhussien@gmail.com>
2024-04-14 10:08:29 +0200

    Test

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -1,6 +1,11 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import 'dotenv/config'
+import {Session} from "./entity/Session";
+import {TollGate} from "./entity/TollGate";
+import {Transaction} from "./entity/Transaction";
+import {User} from "./entity/User";
+import {UserVehicleRequest} from "./entity/UserVehicleRequest";
 
 export const AppDataSource = new DataSource({
   type: process.env.DATABASE_TYPE as "mysql"|"postgres",
@@ -9,7 +14,7 @@ export const AppDataSource = new DataSource({
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: process.env.DATABASE_LOGGING === "true",
-  entities: ["src/entity/**/*.ts"],
+  entities: [Session, TollGate, Transaction, User, UserVehicleRequest],
   migrations: ["src/migration/**/*.ts"],
   subscribers: [],
 })
--- api/src/entity/Session.ts
+++ api/src/entity/Session.ts
@@ -3,7 +3,7 @@
 import { ISession } from "connect-typeorm";
 import { Column, DeleteDateColumn, Entity, Index, PrimaryColumn } from "typeorm";
 
-@Entity()
+@Entity("session")
 export class Session implements ISession {
     @Index()
     @Column("bigint")
