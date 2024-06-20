8181e7f184a6c1a52dd7cfc4a0c64c127078c57f
blank <blankhussien@gmail.com>
2024-06-20 17:38:40 +0300

    fix user vehicle request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,24 +32,24 @@ function App() {
 
   useEffect(() => {
     // TODO: on 401, should redirect to login page and clear user from local storage
-    const getUserFromLocalStorage = () => {
-      const unSerializedUser = localStorage.getItem('user')
-      if (!unSerializedUser) return null
-
-      try {
-        return JSON.parse(unSerializedUser)
-      } catch (error) {
-        localStorage.removeItem('user')
-        return null
-      }
-    }
-
-    const user = getUserFromLocalStorage()
-    if (user && Object.keys(user).length) {
-      setUser(user)
-    } else {
+    // const getUserFromLocalStorage = () => {
+    //   const unSerializedUser = localStorage.getItem('user')
+    //   if (!unSerializedUser) return null
+    //
+    //   try {
+    //     return JSON.parse(unSerializedUser)
+    //   } catch (error) {
+    //     localStorage.removeItem('user')
+    //     return null
+    //   }
+    // }
+    //
+    // const user = getUserFromLocalStorage()
+    // if (user && Object.keys(user).length) {
+    //   setUser(user)
+    // } else {
       fetchUser()
-    }
+    // }
   }, [])
 
   if (loading) {
